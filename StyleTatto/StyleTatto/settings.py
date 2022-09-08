a80524d924daa49a8f426909275de8266e41729b
samuel <samuelelrey0126yt@gmail.com>
2022-09-08 08:23:18 -0500

    modifique

--- StyleTatto/StyleTatto/settings.py
+++ StyleTatto/StyleTatto/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'styletattopython',
         'USER': 'root',
-        'PASSWORD': '123456',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
