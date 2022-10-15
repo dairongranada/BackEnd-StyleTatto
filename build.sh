fee646ec493da446caaf869f496ccb4a7f9abfcb
stevenmontoyat01 <montoyatorressteven0@gmail.com>
2022-10-14 20:54:53 -0500

    se cambio el script de build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python ./StyleTattoo/manage.py collectstatic --no-input
+python ./StyleTattoo/manage.py migrate
