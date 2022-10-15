835f90320db15cfefab56cc3fe66e921aa0a096f
stevenmontoyat01 <montoyatorressteven0@gmail.com>
2022-10-14 21:15:05 -0500

    S.M - se movio build.sh a carpeta raiz

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-pip install -r requirements.txt
-
-python ./StyleTattoo/manage.py collectstatic --no-input
-python ./StyleTattoo/manage.py migrate
